f1be7441369958cc6a9c7709c6dd7af8e1c94835
Shuqi <shuqiyao2017@163.com>
2018-07-16 21:51:38 +0800

    plot done (part I)

--- src/visualization/jinfeng.py
+++ src/visualization/jinfeng.py
@@ -5,13 +5,26 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from dateutil import parser
 
 
 
-jf_sz = pd.read_csv('/Users/yaoshuqi/OneDrive/working/python/prac/stock/data/jf_sc.csv')
-jf_hk = pd.read_csv('/Users/yaoshuqi/OneDrive/working/python/prac/stock/data/jf_hk.csv')
+jf_sz = pd.read_csv('data/jf_sc.csv',parse_dates=['datetime'],index_col=0,date_parser=parser.parse)
+jf_hk = pd.read_csv('data/jf_hk.csv',parse_dates=['datetime'],index_col=0,date_parser=parser.parse)
 # 把日期列转为日期格式，创建时序数据
-jf_sz['datetime'] = jf_sz.datetime.apply(parser.parse)
-jf_sz = jf_sz.set_index('datetime')
+# jf_sz['datetime'] = jf_sz.datetime.apply(parser.parse)
+# jf_sz = jf_sz.set_index('datetime')
+
+#提取出深圳的收盘价
+close_sz = jf_sz['close']
+close_hk = jf_hk['close']
+#画图
+
+# close_sz.plot()
+plt.show(close_sz.plot())
+plt.show(close_hk.plot())
+
+plot_data = pd.concat([close_sz,close_hk],axis=1)
+plot_data.columns = ['close_sz','close_hk']
+plt.show(plot_data.plot())
